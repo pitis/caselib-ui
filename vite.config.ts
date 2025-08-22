5124ef2f6080878b9d9c175d4e7ca55dc8caab48
Pitiș Radu <pitis.radu@protonmail.com>
2025-08-22 16:16:23 +0300

    vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@
 
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 import path from 'path'
+import { defineConfig } from 'vite'
 
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     tailwindcss(),
     react(),
